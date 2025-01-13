038e54092fe9a20c4cfa2748b8bd7be9e6732d43
gabrielnovaisgs <gabrielnovaisgs@gmail.com>
2025-01-13 16:46:05 -0300

    remove mobile navebar

--- src/components/header/mobile-navbar.tsx
+++ src/components/header/mobile-navbar.tsx
@@ -1,6 +1,6 @@
 export default function MobileNavbar() {
     return (
-        <div className="w-4/5 h-screen rounded-r-lg shadow-md p-8 absolute z-50 bg-white top-0 right-0
+        <div className="hidden w-4/5 h-screen rounded-r-lg shadow-md p-8 absolute z-50 bg-white top-0 right-0
             after:content-[''] after:absolute after:top-0 after:left-0 after:w-full after:h-full after:bg-primary/20
         ">
             <h1>MobileNavbar</h1>
